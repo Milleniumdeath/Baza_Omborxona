4c7607b26c4efe5d1df94ea7ec2d668af8840c68
Millenium death <milleniumdeath90@gmail.com>
2025-07-03 13:10:22 +0500

    first commit

---
+++ stats/models.py
@@ -0,0 +1,27 @@
+from users.models import *
+from main.models import *
+
+class IncomeProduct(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    amount = models.FloatField(validators=[MinValueValidator(0.0)], default=1)
+    import_price = models.FloatField(validators=[MinValueValidator(0.0)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE )
+
+    def __str__(self):
+        return self.product.name
+
+class Sale(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    amount = models.FloatField(validators=[MinValueValidator(0.0)], default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.FloatField(validators=[MinValueValidator(0.0)], default=0)
+    paid = models.FloatField(validators=[MinValueValidator(0.0)], default=0)
+    debt = models.FloatField(validators=[MinValueValidator(0.0)], default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+
+    def claculate_total_price(self):
+        self.total_price = self.amount * self.product.export_price
---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from main.models import *
+
+
+class User(AbstractUser):
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=13)
